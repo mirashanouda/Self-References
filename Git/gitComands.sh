033276d200903728fc870abf260041a62b9d4f38
mirashanouda <mira.shanouda@aucegypt.edu>
2024-04-03 11:59:23 +0200

    Git stash documentation

---
+++ Git/gitComands.sh
@@ -0,0 +1,14 @@
+# To stash changes:
+git stash
+
+# To apply stashed changes:
+git stash apply
+
+# To list stashed changes:
+git stash list
+
+# To stash changes with a message:
+git stash push -m "message"
+
+# To apply a specific stash:
+git stash apply stash@{n}
